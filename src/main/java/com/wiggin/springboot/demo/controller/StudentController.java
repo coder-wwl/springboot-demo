ce0ae8df1f9ae5fe9297354e7208f03a3bbff0e9
wwl <wwl_coder@foxmail.com>
2020-02-15 10:47:28 +0800

    提交输出信息dev

--- src/main/java/com/wiggin/springboot/demo/controller/StudentController.java
+++ src/main/java/com/wiggin/springboot/demo/controller/StudentController.java
@@ -37,6 +37,7 @@ public class StudentController {
        student.setName("tony");
        student.setDepartment("纪检部");
        student.setSex("男");
+       System.out.println("this is dev");
        return student;
     }
 
